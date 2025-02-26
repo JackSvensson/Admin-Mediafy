321bbfa84946ffc26cd32692184c7702ee2613c4
Jonatan <j.lorentzson@outlook.com>
2025-02-26 11:48:57 +0100

    Add factory create

--- mediafy/app/Models/Platforms.php
+++ mediafy/app/Models/Platforms.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
 
 class Platforms extends Model
 {
     //
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'type'
+    ];
 }
