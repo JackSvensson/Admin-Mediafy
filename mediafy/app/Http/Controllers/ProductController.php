e839a6100da729c79a1854ab1d07e6df07affd1b
JackSvensson <Jackens1997@gmail.com>
2025-02-27 12:53:07 +0100

    Uppdated productController

--- mediafy/app/Http/Controllers/ProductController.php
+++ mediafy/app/Http/Controllers/ProductController.php
@@ -2,21 +2,21 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use App\Models\Title;
 
 class ProductController extends Controller
 {
 
     // In App\Http\Controllers\ProductController.php
-    public function __invoke(Request $request)
-    {
-        return redirect('/');
-    }
+
 
     public function create(Request $request)
     {
-
-
+        $title = new Title();
+        $title->name = $request->get('title');
+        $title->save();
         return redirect('/');
     }
 }
