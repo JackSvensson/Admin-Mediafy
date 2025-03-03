1baa514984aab6d7f6be6119866dacdc239e7846
JackSvensson <Jackens1997@gmail.com>
2025-03-03 15:56:32 +0100

    Added in code for the create site

--- mediafy/app/Http/Controllers/ProductController.php
+++ mediafy/app/Http/Controllers/ProductController.php
@@ -2,20 +2,40 @@
 
 namespace App\Http\Controllers;
 
-
 use Illuminate\Http\Request;
 use App\Models\Title;
+use App\Models\Platform;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-
-    // In App\Http\Controllers\ProductController.php
-
+    /**
+     * Show form to create a new product
+     */
     public function create(Request $request)
     {
         $title = new Title();
         $title->name = $request->get('title');
         $title->save();
-        return redirect('/');
+
+        // Get platforms from request
+        $platformTypes = $request->input('platforms', []);
+
+        foreach ($platformTypes as $platformType) {
+            // Find or create the platform
+            $platform = Platform::firstOrCreate([
+                'type' => $platformType
+            ]);
+
+            // Create the product with price and stock info
+            $product = new Product();
+            $product->title_id = $title->id;
+            $product->platform_id = $platform->id;
+            $product->price = $request->input('price');
+            $product->stock = $request->input('stock');
+            $product->save();
+        }
+
+        return redirect('/panel');
     }
 }
