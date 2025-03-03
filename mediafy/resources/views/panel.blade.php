faedcd292f6918753f8ece96da5e9072db82ca9c
Jonatan <j.lorentzson@outlook.com>
2025-03-03 15:11:16 +0100

    Add design panel

--- mediafy/resources/views/panel.blade.php
+++ mediafy/resources/views/panel.blade.php
@@ -9,7 +9,7 @@
 
 <body class="bg-neutral-8 text-neutral-1">
     <main class="flex flex-row">
-        <section id="sidebar" class="flex flex-col p-4 w-[250px] bg-neutral-7">
+        <section id="sidebar" class="flex flex-col p-4 w-[250px]">
             <!-- Flex container for sidebar -->
             <div class="flex items-center space-x-2 mb-4">
                 <h1 class="text-xl font-semibold">Admin Panel</h1>
@@ -20,13 +20,49 @@
                 <li>Logout</li>
             </ul>
         </section>
-        <section id="maincontent" class="bg-neutral-5 w-full h-[100vh] px-10">
-            <header class="flex justify-between py-4">
+        <section id="maincontent" class="w-full h-[100vh] px-10">
+            <header class="flex justify-between pt-4  pb-6 items-center">
                 <h1 class="text-xl font-semibold">Product list</h1>
                 <a href="/addproduct" class="bg-primary-1 px-6 py-2 inline-block rounded-md font-medium hover:bg-primary-2"> Add new product</a>
             </header>
-            <div id="filterList">Filter</div>
-            <div id="productsTable">Produkter...</div>
+            <div id="productsTable" class="bg-secondary-1 rounded-md p-4 border-1 border-neutral-6">
+                <h2 class="text-xl pb-4">Alla produkter</h2>
+                <table class="min-w-full table-auto">
+                    <thead class="text-left">
+                        <tr>
+                            <th class="px-4 py-2">Title</th>
+                            <th class="px-4 py-2">Platform</th>
+                            <th class="px-4 py-2">Price</th>
+                            <th class="px-4 py-2">Antal i lager</th>
+                            <th class="px-4 py-2"></th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td class="px-4 py-2">Star wars</td>
+                            <td class="px-4 py-2">Playstation</td>
+                            <td class="px-4 py-2">359kr</td>
+                            <td class="px-4 py-2">2st</td>
+                            <td class="px-4 py-2">
+                                <a href="/edit" class="text-blue-500">Edit</a> |
+                                <a href="/delete" class="text-red-500">Delete</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="px-4 py-2">Halo</td>
+                            <td class="px-4 py-2">Xbox</td>
+                            <td class="px-4 py-2">200kr</td>
+                            <td class="px-4 py-2">10st</td>
+                            <td class="px-4 py-2">
+                                <a href="/edit" class="text-blue-500">Edit</a> |
+                                <a href="/delete" class="text-red-500">Delete</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
         </section>
     </main>
 </body>
