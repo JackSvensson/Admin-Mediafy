932a0ca6ecec4be3d6a96fa83e231f23766a6993
JackSvensson <Jackens1997@gmail.com>
2025-03-05 09:15:46 +0100

    Updated middleware

--- mediafy/routes/web.php
+++ mediafy/routes/web.php
@@ -17,11 +17,11 @@ Route::get('logout', LogoutController::class);
 //Senare lägg till group för alla auth /panels
 Route::get('/panel', [PanelController::class, 'index'])->middleware("auth");
 
-Route::get('/panel/addproduct', [PanelController::class, 'addproduct']);
+Route::get('/panel/addproduct', [PanelController::class, 'addproduct'])->middleware("auth");
 
 Route::delete('/panel/product{id}/delete', [PanelController::class, 'deleteProduct'])->name("delete");
 
-Route::get('/panel/product/{id}/edit', [PanelController::class, 'editProduct'])->name('panel.product.edit');
-Route::put('/panel/product/{id}/update', [PanelController::class, 'updateProduct'])->name('panel.product.update');
+Route::get('/panel/product/{id}/edit', [PanelController::class, 'editProduct'])->name('panel.product.edit')->middleware("auth");
+Route::put('/panel/product/{id}/update', [PanelController::class, 'updateProduct'])->name('panel.product.update')->middleware("auth");
 
 Route::post('/product', [ProductController::class, 'create']);
